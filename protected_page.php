d93df005f78d72fe98c902f62dcca27801b0a512
HD Koh <hd.koh290293@gmail.com>
2016-05-27 15:34:21 +0800

    updated loginpage

--- protected_page.php
+++ protected_page.php
@@ -1,7 +1,23 @@
 <?php
+/**
+ * Copyright (C) 2013 peredur.net
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
- 
+
 sec_session_start();
 ?>
 <!DOCTYPE html>
@@ -13,7 +29,7 @@ sec_session_start();
     </head>
     <body>
         <?php if (login_check($mysqli) == true) : ?>
-            <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
+        <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
             <p>
                 This is an example protected page.  To access this page, users
                 must be logged in.  At some stage, we'll also check the role of
