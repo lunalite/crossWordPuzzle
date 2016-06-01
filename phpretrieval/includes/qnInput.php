4a797033f0b0b820743f0af285b979b719bd6ef7
w31ha0 <lewweihao93@hotmail.com>
2016-06-01 13:08:43 +0800

    Update qnInput.php

--- phpretrieval/includes/qnInput.php
+++ phpretrieval/includes/qnInput.php
@@ -1,7 +1,6 @@
 <?php
     include_once 'php-connect.php';
     include_once 'phpVariables.php';
-
     //echo $_POST["questions"]."<br>";
     $string = $_POST["questions"];
     $questions = preg_split("/\d\)\s+/",$string);
@@ -9,12 +8,17 @@
     //echo $questions[1] . "<br>";
     //echo $questions[2] . "<br>";
     //echo $questions[3] . "<br>";
-
     // Checks for the first id from crosswordmasterdb.
+    // Inserting into master database id of the new crossword to be added.
+    $sql = "INSERT INTO " . $tableName . " (crosswordId) VALUES (0)";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully" . "<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
     $sql = "SELECT crosswordId FROM " . $tableName;
     $result = $conn->query($sql);
     $latestCrossWordId = 0;
-
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
@@ -28,17 +32,8 @@
         //echo "0 results";
         $latestCrossWordId --;
     }
-    $latestCrossWordId ++;
     echo $latestCrossWordId . "<br>";
-
-    // Inserting into master database id of the new crossword to be added.
-    $sql = "INSERT INTO " . $tableName . " (crosswordId) VALUES (" . $latestCrossWordId . ")";
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully" . "<br>";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
+    
     // Inserting all the questions and answers into the crossword bank
     $counter = 1;
     foreach ($questions as $qn2BAdded) {
@@ -49,10 +44,8 @@
         
         $qn2BAddedAgain = preg_split("/".$answer[0]."/", $qn2BAdded);
         echo $qn2BAddedAgain[0] . "<br>";
-
-        $sql = "INSERT INTO " . $crosswordBankName . " VALUES ("
-        . $counter . ", \"" . $qn2BAddedAgain[0] . "\", \"" . $answer[0] . "\", " . $latestCrossWordId . ")";
-
+        $sql = "INSERT INTO " . $crosswordBankName . " VALUES (0," . $latestCrossWordId . ", "
+        . $counter . ",\"" . $qn2BAddedAgain[0] . "\", \"" . $answer[0] . "\",\"" . $answer[0] . "\")";
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully" . "<br>";
         } else {
@@ -62,6 +55,5 @@
         $counter ++;
     }
     unset($qn2BAdded); // break the reference with the last element
-
-        header('location: ../crosswordGen.php');   
-?>
+        header("location:master_template.php?id=".urlencode($latestCrossWordId));   
+?>		
