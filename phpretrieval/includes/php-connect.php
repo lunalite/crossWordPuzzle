8ffa6b03aab27bc0c781cbc0fa9d5f1b78d37af2
w31ha0 <lewweihao93@hotmail.com>
2016-06-01 13:07:37 +0800

    Update php-connect.php

--- phpretrieval/includes/php-connect.php
+++ phpretrieval/includes/php-connect.php
@@ -1,14 +1,12 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "eKcGZr59zAa2BEWU";
-    $dbname = "xword";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
+ $host="mysql10.000webhost.com"; // Host name 
+$username="a7390942_w31ha0"; // Mysql username 
+$password="cultivateta0"; // Mysql password 
+$db_name="a7390942_test"; // Database name 
+ 
+ $conn = new mysqli($host, $username, $password, $db_name);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-        //echo "Connected successfully." . "<br>";
     
 ?>
