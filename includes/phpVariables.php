7ea62a30894a1e97b2ae438bfa1f4ccf469d2a57
HD Koh <hd.koh290293@gmail.com>
2016-06-26 19:40:20 +0800

    release1.1

---
+++ includes/phpVariables.php
@@ -0,0 +1,20 @@
+<?php
+    
+    // Table names
+    $members = "members";
+    $loginAttempts = "login_attempts";
+    $crosswordMaster = "crosswordmasterdb";
+    $crosswordPuzzles = "crosswordpuzzles";
+    $availableSessions = "availablesessions";
+    $sessionJoin = "sessionJoin";
+
+    /* Session names
+    ** $_SESSION['user_id']
+    ** $_SESSION['username']
+    ** $_SESSION['login_string']
+    ** $_SESSION['permissions']
+    ** $_SESSION['sess_id']
+    */
+    
+?>
+
