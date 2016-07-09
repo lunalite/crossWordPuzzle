4e46eb2f9a999733318c1a69d7e2a2da13af851d
luna <hd.koh290293@gmail.com>
2016-07-09 13:27:39 -0700

    Patch 2.0 Included groups and various other changes

---
+++ users/groups.php
@@ -0,0 +1,126 @@
+<?php
+    include_once '../includes/db_connect.php';
+    include_once '../includes/functions.php';
+    
+    sec_session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>REP Xword Group Management page</title>
+        <link href="../css/bootstrap.css" rel="stylesheet">
+        <link href="../css/jumbotron.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script src="../css/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="../css/js/ie10-viewport-bug-workaround.js"></script>
+        <script>
+            $(function () {
+                $('#userList').find("tr").click(function () {
+            
+                    var url = "./grantUser.php?userId=" + this.id;
+                    window.location.href = url;
+                });
+            });
+            
+        </script>
+    </head>
+    <body>
+        <?php if ((login_check($mysqli) == true) && role_check($mysqli) == 2) : ?> 
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                  <a class="navbar-brand" href="../master.php" style="color:white;">REP Crossword Group Management Page</a>
+                </div>
+                <div id="navbarCollapse" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="../crosswords/crosswords.php" style="color:white;">Crosswords</a></li>
+                        <li><a href="../reviews/reviews.php" style="color:white;">Reviews</a></li>
+                        <li><a href="./users.php" style="color:white;">Users</a></li>
+                                                    <!--
+                                                            <li class="dropdown">
+                                                                <a data-toggle="dropdown" class="dropdown-toggle" href="#">Messages <b class="caret"></b></a>
+                                                                <ul role="menu" class="dropdown-menu">
+                                                                    <li><a href="#">Inbox</a></li>
+                                                                    <li><a href="#">Drafts</a></li>
+                                                                    <li><a href="#">Sent Items</a></li>
+                                                                    <li class="divider"></li>
+                                                                    <li><a href="#">Trash</a></li>
+                                                                </ul>
+                                                            </li> -->
+                    </ul>
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <div class="navbar-right navbar-form" style="color:white;">
+
+                        <?php loginNavBarAction($mysqli); ?>
+
+                        <a class="btn btn-success" href="../includes/logout.php" role="button">Log out</a>
+                        </div>
+
+                    </div><!--/.navbar-collapse -->
+                </div>
+            </div>
+        </nav>
+
+<!-- Only for admins -->
+
+        <div class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-8 col-md-offset-2">
+
+                        <form action="./newGroup.php">
+                            <input type="submit" class="btn btn-danger btn-sm" value="Create Group">
+                        </form>
+
+                        <h3>All groups</h3>
+                        <table id="userList" class="table table-striped table-hover">
+                            <thead>
+                            <tr>
+                                <th>classGroup ID</th>
+                                <th>classGroup Name</th>
+                            </tr>
+                            </thead>
+                            <tbody style="cursor: pointer;">
+                                <?php
+                                    groupCheck($mysqli);
+                                ?>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                    <div class="col-xs-12 col-md-8 col-md-offset-2">
+                <hr><p>&copy; 2016 Product of REP</p>
+</div>
+            </div>
+        </div>
+
+        <!--**********************************************************************-->
+        <!-- For the case of wrong login -->
+        <?php
+            elseif ((login_check($mysqli) == true) && role_check($mysqli) == 0)  :
+                       echo '<script>';
+                       echo 'window.location.href="../user.php"';
+                       echo '</script>';
+            elseif ((login_check($mysqli) == true) && role_check($mysqli) == 1)  :
+                       echo '<script>';
+                       echo 'window.location.href="../master.php"';
+                       echo '</script>';
+                   else :
+                       echo '<script>';
+                       echo 'window.location.href="../index.php"';
+                       echo '</script>';
+            
+                   endif;
+        ?>
+    </body>
+</html>
---
+++ users/includes/groupCreation.php
@@ -0,0 +1,25 @@
+<?php
+
+    include_once '../../includes/db_connect.php';
+    include_once '../../includes/functions.php';
+    
+    sec_session_start();
+
+    $groupName = $_POST['groupName'];
+    $checkQuery = "SELECT * FROM ".$GLOBALS['classGroup']." WHERE classGroupName = '".$groupName."'";
+    $checkResult = $mysqli->query($checkQuery );
+
+    if(mysqli_num_rows($checkResult) > 0) {
+        header("Refresh:3; url=../groups.php");
+        echo 'Repeated group name. Please use another. Going back in 3...';
+    } else {
+        $sql = "INSERT INTO ".$GLOBALS['classGroup']. " (classGroupName) VALUES ('" . $groupName. "')";
+       /* if ($mysqli->query($sql) === TRUE) {
+            header("Refresh:3; url=../groups.php");
+            echo "New group created successfully. Going back in 3..." . "<br>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $mysqli->error;
+    }*/
+    
+}
+?>
